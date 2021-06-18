02301951422dd51b16c46667e1b13cc7c61d293c
Roger.Farber <rogerfarber@gmail.com>
2021-06-18 12:51:51 +0300

    Imported
    Just imported vending machine

---
+++ Vending/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Timer;
+import java.util.TimerTask;
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+        Machine myMachine = new Machine();
+        myMachine.start();
+
+        System.out.println("-----------timeout-----------");
+        Thread.sleep(6000);
+
+        System.out.println("-----------Get product-----------");
+        myMachine.start();
+        myMachine.insertCoin(Machine.Coins.SHEKEL);
+        myMachine.insertCoin(Machine.Coins.SHNEKEL);
+        myMachine.insertCoin(Machine.Coins.SHNEKEL);
+        myMachine.getProduct();
+
+        System.out.println("-----------Cancel-----------");
+        myMachine.start();
+        myMachine.insertCoin(Machine.Coins.SHEKEL);
+        myMachine.insertCoin(Machine.Coins.SHNEKEL);
+        myMachine.insertCoin(Machine.Coins.SHNEKEL);
+        myMachine.cancel();
+
+    }
+}
